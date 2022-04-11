329963ca270442582e91722f8b898792ce26fc19
Rangga Prangwedana Januwarsa <54941482+rangga-prangwedana@users.noreply.github.com>
2022-04-11 10:12:15 +0700

    Add files via upload

---
+++ tlx-toki/dasar/bab-11/operasi.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    string s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+    s1.erase(s1.find(s2), s2.length());
+    s1.insert(s1.find(s3) + s3.length(), s4);
+
+    cout << s1 << "\n";
+
+    return 0;
+}
