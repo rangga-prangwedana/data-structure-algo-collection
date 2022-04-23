7b71765562a2c89b426958596ba7bfdf558e6748
Rangga Prangwedana Januwarsa <54941482+rangga-prangwedana@users.noreply.github.com>
2022-04-23 11:25:05 +0700

    Add files via upload

---
+++ codechef/1-star/0-basic/distinct-pair-sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T;
+	cin >> T;
+	while (T--)
+	{
+	    int l, r;
+	    cin >> l >> r;
+	    cout << 2 * r - 2 * l + 1 << "\n";
+	}
+	
+	return 0;
+}
