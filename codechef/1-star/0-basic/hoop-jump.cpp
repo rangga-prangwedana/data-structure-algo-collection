09b2e271b2645b306fcbc8bab5d0306f4805f319
Rangga Prangwedana Januwarsa <54941482+rangga-prangwedana@users.noreply.github.com>
2022-04-24 11:22:46 +0700

    Add files via upload

---
+++ codechef/1-star/0-basic/hoop-jump.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T;
+	cin >> T;
+	while (T--)
+	{
+	    int n;
+	    cin >> n;
+	    cout << n / 2 + 1 << "\n";
+	}
+	
+	return 0;
+}
